b3d1bd1c11719cb0817c0615505e376f82c8fa90
Claymore121 <92693513+Claymore121@users.noreply.github.com>
2022-10-04 10:09:03 -0700

    Delete juegoPares directory

--- juegoPares/js/game-over.js
+++
@@ -1,9 +0,0 @@
-function gameOver() {
-    clearInterval(cronometro);
-    document.querySelector("#gameOver").classList.add("visible");
-  }
-  
-  function timeOver() {
-    document.querySelector("#timeOver").classList.add("visible");
-  }
-  
